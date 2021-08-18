c97711651b35054239031399ac43c36002e1fe0f
Dmytro Strelnikov <d.strelnikov@inventati.org>
2021-08-18 14:32:08 +0200

    composition: cosmetics

--- hydrosolver/composition.py
+++ hydrosolver/composition.py
@@ -19,7 +19,7 @@ nutrients_stencil = [
 ]
 
 
-def load_db(database_dict):
+def load_compositions(database_dict):
     compositions = {
             name: load({name: nutrients})
             for name, nutrients in database_dict.items()
@@ -28,7 +28,7 @@ def load_db(database_dict):
     return compositions
 
 def load( composition_dict):
-    '''Load composition from a dict.'''
+    '''Creates a new Composition from a dict.'''
 
     name, nutrients_dict = tuple(composition_dict.items())[0]
     vector = np.zeros(len(nutrients_stencil))
@@ -46,8 +46,36 @@ class Composition:
         self.name = name
         self.vector = vector
 
+    def __add__(self, composition):
+        name = f'{self.name} + {composition.name}'
+        vector = self.vector + composition.vector
+
+        return Composition(name, vector)
 
     def __repr__(self):
+        return self.as_table_plain()
+
+    def __str__(self):
+        return self.as_string_plain()
+
+    def __rmul__(self, number):
+        name = f'{number} * ({self.name})'
+        vector = self.vector * number
+
+        return Composition(name, vector)
+
+    def dump(self):
+        '''Dumps a Composition into a dict.'''
+
+        nutrients_dict = {
+                nutrient: value
+                for nutrient, value in zip(nutrients_stencil, self.vector)
+                if value != 0
+                }
+
+        return {self.name: nutrients_dict}
+
+    def as_table_plain(self):
 
         description = f'Composition: {self.name}'
 
@@ -64,7 +92,7 @@ class Composition:
 
         return '\n\n'.join((description, table))
 
-    def __str__(self):
+    def as_string_plain(self):
         nutrients_list = (
                 f'{desc}: {value}'
                 for (desc, value) in zip(nutrients_stencil, self.vector)
@@ -72,26 +100,3 @@ class Composition:
                 )
 
         return ' :: '.join((self.name, ', '.join(nutrients_list)))
-
-    def __add__(self, composition):
-        name = f'{self.name} + {composition.name}'
-        vector = self.vector + composition.vector
-
-        return Composition(name, vector)
-
-    def __rmul__(self, number):
-        name = f'{number} * ({self.name})'
-        vector = self.vector * number
-
-        return Composition(name, vector)
-
-    def dump(self):
-        '''Represent a composition as a dict.'''
-
-        nutrients_dict = {
-                nutrient: value
-                for nutrient, value in zip(nutrients_stencil, self.vector)
-                if value != 0
-                }
-
-        return {self.name: nutrients_dict}
