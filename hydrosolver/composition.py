3679ca2171818100a0e934afdfe9208081c149af
Dmytro Strelnikov <d.strelnikov@inventati.org>
2021-08-18 14:10:44 +0200

    composition: adjust representations to the new format

--- hydrosolver/composition.py
+++ hydrosolver/composition.py
@@ -60,36 +60,30 @@ class Composition:
                 self.vector[i] = nutrients_dict[nutrient]
 
     def __repr__(self):
+
         description = f'Composition: {self.name}'
 
-        table_macro = tabulate(
-                [[desc, amount, amount * 10**6] for (desc, amount) in
-                            zip(macronutrients_desc, self.macronutrients)],
-                headers=['Macronutrient', 'Ratio', 'Amount mg/kg'],
+        table = tabulate(
+                [
+                    (nutrient, value, value * 10**6)
+                    for (nutrient, value)
+                    in zip(nutrients_stencil, self.vector)
+                    if value != 0
+                ],
+                headers=['Nutrient', 'Ratio', 'Amount mg/kg'],
                 tablefmt='simple',
                 )
 
-        table_micro = tabulate(
-                [[desc, amount, amount * 10**6] for (desc, amount) in
-                            zip(micronutrients_desc, self.micronutrients)],
-                headers=['Micronutrient', 'Ratio', 'Amount mg/kg'],
-                tablefmt='simple',
-                )
-
-        return '\n\n'.join([description, table_macro, table_micro])
+        return '\n\n'.join((description, table))
 
     def __str__(self):
-        list_macro = (f'{desc}: {amount}' for (desc, amount) in
-                            zip(macronutrients_desc, self.macronutrients))
-        list_micro = (f'{desc}: {amount}' for (desc, amount) in
-                            zip(micronutrients_desc, self.micronutrients))
+        nutrients_list = (
+                f'{desc}: {value}'
+                for (desc, value) in zip(nutrients_stencil, self.vector)
+                if value != 0
+                )
 
-        return '\n'.join(
-                (
-                    self.name,
-                    ', '.join(list_macro),
-                    ', '.join(list_micro)
-                ))
+        return ' :: '.join((self.name, ', '.join(nutrients_list)))
 
     def __add__(self, composition):
         name = f'{self.name} + {composition.name}'
@@ -100,7 +94,7 @@ class Composition:
 
     def __rmul__(self, number):
         name = f'{number} * ({self.name})'
-        macronutrients = self.macronutrients * number
+        vector = self.macronutrients * number
         micronutrients = self.micronutrients * number
 
         return Composition(macronutrients, micronutrients, name)
