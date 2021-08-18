73f569048aa16490a3a876ffab7ca084065e78f7
Dmytro Strelnikov <d.strelnikov@inventati.org>
2021-08-18 16:11:13 +0200

    solution: allow negative values in the formulations
    In order to complete the space of solutions w.r.t. vector additions and
    scalar multiplication.

--- hydrosolver/solution.py
+++ hydrosolver/solution.py
@@ -28,14 +28,6 @@ class Solution:
             raise ValueError(
                 'The formulation does not match the number of fertilizers.')
 
-        if np.any(formulation < 0):
-            raise ValueError(
-                'The formulation contains negative values.')
-
-        if formulation.sum() > mass_total:
-            raise ValueError(
-                'The mass of the fertilizers is greater than the total mass.')
-
         self.mass_total = mass_total
         self.water = water
         self.formulation = formulation
