56681e4566a398c602f9f71c8605675158eac84c
Dmytro Strelnikov <d.strelnikov@inventati.org>
2021-08-31 17:57:30 +0200

    core: add weight vector to the gradient evaluation

--- hydrosolver/core.py
+++ hydrosolver/core.py
@@ -6,12 +6,12 @@ import numpy as np
 def residual(A, b, x):
     return A @ x - b
 
-def gradient(A, b, x):
+def gradient(A, b, x, w):
     '''Computes the gradient of the least square objective functional.
         A x -> b
 
     '''
-    return 2 * (A @ x - b).transpose() @ A
+    return 2 * w * (A @ x - b).transpose() @ A
 
 def solve_lstsq(A, b):
     return np.linalg.lstsq(A, b, rcond=None)[0]
