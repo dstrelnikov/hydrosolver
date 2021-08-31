0a9b4cd9908a02ef469384ff91a3b769bb238202
Dmytro Strelnikov <d.strelnikov@inventati.org>
2021-08-31 17:56:53 +0200

    utils: refactor

--- hydrosolver/utils.py
+++ hydrosolver/utils.py
@@ -3,6 +3,6 @@ from .composition import load_compositions
 
 def load_file(filename):
     with open(filename) as file:
-        compositions_list = load_compositions(yaml.safe_load(file))
+        compositions_dict = load_compositions(yaml.safe_load(file))
 
-    return compositions_list
+    return compositions_dict
