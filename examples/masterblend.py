8d00f51171ba2eceeee7c8278a2d49e66a6fe5fa
Dmytro Strelnikov <d.strelnikov@inventati.org>
2021-09-06 14:08:02 +0200

    examples: cosmetics

--- examples/masterblend.py
+++ examples/masterblend.py
@@ -8,24 +8,22 @@ masterblend = utils.load_file('compositions/masterblend.yaml')
 
 # https://hydroponicseuro.com/mixing-instructions/
 
+compositions = [
+        masterblend['Masterblend 4-18-38 Tomato Formula'],
+        pure['Calcium nitrate tetrahydrate'],
+        pure['Magnesium sulfate heptahydrate'],
+    ]
+
 leafy_greens = Solution.dissolve(
         10,
         Composition(name='RO water'),
-        [
-            masterblend['Masterblend 4-18-38 Tomato Formula'],
-            pure['Calcium nitrate tetrahydrate'],
-            pure['Magnesium sulfate heptahydrate'],
-        ],
+        compositions,
         [0.0053, 0.0053, 0.00265],
     )
 
 fruit_bearing = Solution.dissolve(
         10,
         Composition(name='RO water'),
-        [
-            masterblend['Masterblend 4-18-38 Tomato Formula'],
-            pure['Calcium nitrate tetrahydrate'],
-            pure['Magnesium sulfate heptahydrate'],
-        ],
+        compositions,
         [0.0063, 0.0063, 0.00315],
     )
